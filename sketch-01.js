5b2b37b4f741486ced2218612dd6b086f8cd9429
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-05-08 23:47:17 +0530

    Grid Art

--- sketch-01.js
+++ sketch-01.js
@@ -1,13 +1,45 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [1000, 1000]
+
+  // dimensions: 'A4',
+  // pixelsPerInch: 300,
+  // orientation: 'landscape'
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+    context.lineWidth = width * 0.01;
+
+        const w  = width * 0.10;
+        const h = height * 0.10;
+        const gap = width * 0.03;
+        const ix = width * 0.17;
+        const iy = height * 0.17;
+        let x, y;
+
+        const off = width * 0.02;
+
+        for(j = 0; j < 5; j++) {
+            for (i = 0; i < 5; i++) {
+                
+                x = ix + (w + gap) * j;
+                y = iy + (w + gap) * i;
+
+                context.beginPath();
+                context.rect(x, y, w, h)
+                context.stroke();
+
+                if (Math.random() < 0.5) {
+                    context.beginPath();
+                    context.rect(x + off / 2, y + off / 2, w - off, h - off);
+                    context.stroke();
+                }
+            }
+        }
   };
 };
 
